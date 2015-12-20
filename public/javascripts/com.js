1924d2e7d4dfc3d34d22521dd273779007a2c87f
root <justin.tyler.coon@gmail.com>
2015-12-20 12:19:54 -0500

    added sockets

---
+++ public/javascripts/com.js
@@ -0,0 +1,10 @@
+var socket = io();
+$('form').submit(function(){
+socket.emit('msg', $('#m').val());
+$('#m').val('');
+return false;
+});
+socket.on('msg', function(msg){
+$('#messages').append($('<li>').text(msg));
+});
+</script>
